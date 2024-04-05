10ace29be299684afbc887494393dd4f2b9a1418
SyedNathim <syednathim@gmail.com>
2024-04-05 10:34:13 +0530

    change category api url link

--- src/Constants/URL.js
+++ src/Constants/URL.js
@@ -1,5 +1,5 @@
 export const SIGNUP_API_URL = "http://127.0.0.1:8000/api/userRegister";
-export const CATEGORYSHOW_API_URL = "http://localhost/php/crud/category_show.php?function=categoryShow";
+export const CATEGORYSHOW_API_URL = "http://127.0.0.1:8000/api/categoryShow";
 export const HOMECATEGORY_API_URL = "http://localhost/php/crud/category_show.php?function=homeCategory";
 
 export const FETCH_API_URL = "http://localhost/php/crud/api.php?function=getUsers";
